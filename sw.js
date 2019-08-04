9a7853bcad019faa08fd42d9a3d5c731e605306f
migueldeguzmandev <migueldeguzmandev@gmail.com>
2019-08-03 21:49:01 -0500

    maria helena

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f7e8cf68c871a42d084e.js"
+    "url": "webpack-runtime-4af6a129cb8694f5a40b.js"
   },
   {
     "url": "styles.1920d5349338c1f98e01.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d68366a4cac5526ba4ce5bf72b09774b"
+    "revision": "d3f0d3f58f7d9d2bba1499a0a94f419f"
   },
   {
     "url": "component---src-pages-404-js-571ae18d97697a6a1ba2.js"
