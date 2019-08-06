1dfd3d2c9e8f81b84c94cc5d14d5503dd9db8a32
migueldeguzmandev <migueldeguzmandev@gmail.com>
2019-08-05 19:40:46 -0500

    changing new picture

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0aec469c93b3cab9f90b.js"
+    "url": "webpack-runtime-8724ae0789a26f4eaad3.js"
   },
   {
     "url": "styles.1920d5349338c1f98e01.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6f7b16732d2d7d4496aadb878de36483"
+    "revision": "111538a3e537c96a33e3674503cc66b7"
   },
   {
     "url": "component---src-pages-404-js-571ae18d97697a6a1ba2.js"
