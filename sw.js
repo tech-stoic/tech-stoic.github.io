e6c7326b9f178e2e85d1871575e830f6bff6841b
migueldeguzmandev <migueldeguzmandev@gmail.com>
2019-08-07 07:53:52 -0500

    update: latakz blog

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1854e9fbb70e16747ffd.js"
+    "url": "webpack-runtime-34851867509ba6a1887b.js"
   },
   {
     "url": "styles.1920d5349338c1f98e01.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fb91763e5f5e691816ba6e835d63e373"
+    "revision": "4dde27cf796d465325ccdd455069a72f"
   },
   {
     "url": "component---src-pages-404-js-571ae18d97697a6a1ba2.js"
