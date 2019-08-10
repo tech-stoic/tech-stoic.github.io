6ee2c4859ce2f2fa6cadcae310b8c4973f401d7b
migueldeguzmandev <migueldeguzmandev@gmail.com>
2019-08-10 15:48:49 -0500

    letter-to-my-future-wife

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f56a79df3ad11fd855ae.js"
+    "url": "webpack-runtime-431ecea5c80f4db66316.js"
   },
   {
     "url": "styles.1920d5349338c1f98e01.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "eed2312bc3778b4bfe3d9178b8d2ba2f"
+    "revision": "d3c03d1c45af034e79c9b3c733b570a0"
   },
   {
     "url": "component---src-pages-404-js-571ae18d97697a6a1ba2.js"
