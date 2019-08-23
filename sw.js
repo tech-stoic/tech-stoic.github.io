fb830a8a33d8e354567ab1cec21b2361910528ea
migueldeguzmandev <migueldeguzmandev@gmail.com>
2019-08-22 20:36:03 -0500

    new blog: just my resources

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-429d55a0e892b7e1272e.js"
+    "url": "webpack-runtime-a9865c6989627c7024fb.js"
   },
   {
     "url": "styles.1920d5349338c1f98e01.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "07c3bc02062032d1e0183471b0b8cfd8"
+    "revision": "6425c35601881031e56c6e80230dc394"
   },
   {
     "url": "component---src-pages-404-js-571ae18d97697a6a1ba2.js"
