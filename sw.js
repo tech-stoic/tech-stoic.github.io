70e5ca8ed82a18153f6561a3a3a856c3b7fa21e2
migueldeguzmandev <migueldeguzmandev@gmail.com>
2019-09-03 19:14:51 -0500

    new blog

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0af3da1d9e757bde601e.js"
+    "url": "webpack-runtime-fc80e29b4299779a6ac2.js"
   },
   {
     "url": "styles.1920d5349338c1f98e01.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5831526c6fb4a6a77ded7b3bbd38bac3"
+    "revision": "218c8187338ebdd50fe5bc91fb9f1d8a"
   },
   {
     "url": "component---src-pages-404-js-571ae18d97697a6a1ba2.js"
