66af9be0995bbcb871acc3bab1904e4332a600cf
migueldeguzmandev <migueldeguzmandev@gmail.com>
2019-09-04 20:10:26 -0500

    serpents

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fc80e29b4299779a6ac2.js"
+    "url": "webpack-runtime-dba044f429b5f52e14d4.js"
   },
   {
     "url": "styles.1920d5349338c1f98e01.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "218c8187338ebdd50fe5bc91fb9f1d8a"
+    "revision": "481c3118c8affbdc21c5a9ec8fbf6694"
   },
   {
     "url": "component---src-pages-404-js-571ae18d97697a6a1ba2.js"
