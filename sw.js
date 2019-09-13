6fd7b9698982404b76d92a786d5fb97b82f30a5e
migueldeguzmandev <migueldeguzmandev@gmail.com>
2019-09-13 18:25:19 -0500

    guitar ep1

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c87bba205fa1c437b578.js"
+    "url": "webpack-runtime-009aa705e0aee9941810.js"
   },
   {
     "url": "styles.1920d5349338c1f98e01.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d438097b95fb9bf022f28e1d859972f4"
+    "revision": "7495a0b8f28957e6b5c2034db1c03a36"
   },
   {
     "url": "component---src-pages-404-js-571ae18d97697a6a1ba2.js"
