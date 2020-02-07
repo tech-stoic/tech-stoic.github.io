b2cac3980654330d6d43c175a872bc70fc7fe492
migueldeguzmandev <migueldeguzmandev@gmail.com>
2020-02-06 22:36:46 -0500

    update on about page

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ee838fa36eeaeb877165.js"
+    "url": "webpack-runtime-7479a1f6d842f9be5c43.js"
   },
   {
     "url": "styles.1920d5349338c1f98e01.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "926a147dcd2533c95f1e49e47c81d8e9"
+    "revision": "8435cd22a71d1903aed11c33425d44e6"
   },
   {
     "url": "component---src-pages-404-js-f1243e952adee72cfdc2.js"
